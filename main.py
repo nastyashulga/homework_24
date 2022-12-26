984fa50a83390fdd45c80e6334f4de572d38156f
nastiashulga <nastyashulga21@mail.ru>
2022-12-26 14:47:48 +0300

    finished homework

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from views import main_bp
+
+
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(main_bp)
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(port=14000)
